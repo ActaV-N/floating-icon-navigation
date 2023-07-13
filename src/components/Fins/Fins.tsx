0cf70837dcc72949b233cf3c116d79e27f2de07a
Lee <actav@Leeui-MacBookPro.local>
2023-07-13 15:33:49 +0900

    Fin style

--- src/components/Fins/Fins.tsx
+++ src/components/Fins/Fins.tsx
@@ -1,7 +1,46 @@
 'use client';
 
-function Fins() {
+import styled from '@emotion/styled';
+
+const FinsContainer = styled.div`
+  max-width: 310px;
+  width: fit-content;
+  overflow: auto;
+`;
+
+const FinsWrapper = styled.div`
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-auto-flow: column;
+  column-gap: 15px;
+
+  justify-content: space-between;
+
+  background: rgba(255, 255, 255, 0.6);
+  border-radius: 24px;
+
+  padding: 0 10px;
+`;
+
+export interface FinsProps {
+  children: React.ReactNode;
+}
+
+/**
+ * Fins Component, children should be a list of Fin component
+ * @component
+ * @example
+ * return (
+ *   <Fins>
+ *     <Fin `props` />
+ *     <Fin `props` />
+ *     <Fin `props` />
+ *   </Fins>
+ * )
+ */
+function Fins(props: FinsProps) {
   // prop destruction
+  const { children } = props;
 
   // lib hooks
 
@@ -17,7 +56,11 @@ function Fins() {
 
   // handlers
 
-  return <div></div>;
+  return (
+    <FinsContainer>
+      <FinsWrapper>{children}</FinsWrapper>
+    </FinsContainer>
+  );
 }
 
 export { Fins };
--- src/stories/Fins.stories.tsx
+++ src/stories/Fins.stories.tsx
@@ -1,12 +1,27 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { ComponentProps } from 'react';
-import { Fins } from '~components';
+import { AiFillAlert, AiFillAlipaySquare, AiFillAmazonCircle } from 'react-icons/ai';
+import { Fins, Fin } from '~components';
 
 type ArgTypes = ComponentProps<typeof Fins>;
+
+const wrapper = (storyFn: any) => (
+  <div style={{ background: '#1D1D1C', width: '100vw', height: '100vh' }}>{storyFn()}</div>
+);
 
 export default {
   title: 'Components/Fins',
   component: Fins,
+  decorators: [wrapper],
+  args: {
+    children: (
+      <>
+        <Fin icon={<AiFillAlert />} />
+        <Fin icon={<AiFillAlipaySquare />} />
+        <Fin icon={<AiFillAmazonCircle />} />
+      </>
+    ),
+  },
 } as Meta<ArgTypes>;
 
 export const Default: StoryObj<ArgTypes> = {};
