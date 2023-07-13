4fee1b2a26462397b8c6ae64bdeb59e3c66a076c
Lee <actav@Leeui-MacBookPro.local>
2023-07-13 17:26:18 +0900

    updated provider

--- src/components/FinProvider/FinProvider.tsx
+++ src/components/FinProvider/FinProvider.tsx
@@ -3,21 +3,29 @@
 import React, { createContext, useEffect, useRef, useState } from 'react';
 import { Subject } from 'rxjs';
 
+type EventType = 'start' | 'end' | 'setting' | 'register';
 export interface Event {
-  type: 'start' | 'end' | 'setting';
+  type: EventType;
   currentPath: string;
   nextPath?: string;
+}
+
+export interface EventHandler {
+  type: EventType;
+  handler: (event: Event, ...args: any[]) => void;
 }
 
 export interface FinContext {
   next: (event: Event) => void;
   currentPath: string;
   nextPath?: string;
+  register: (handler: EventHandler) => void;
 }
 
 export const FinContext = createContext<FinContext>({
   next: () => {},
   currentPath: '',
+  register: () => {},
 });
 
 export interface FinProviderProps {
@@ -34,19 +42,30 @@ function FinProvider(props: FinProviderProps) {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [currentPath, setCurrentPath] = useState<string>('');
   const finSubject = useRef<Subject<Event>>(new Subject());
+  const handlers = useRef<EventHandler[]>([]);
 
   // form hooks
 
   // query hooks
 
   // calculated values
+  const register = (eventHandler: EventHandler) => handlers.current.push(eventHandler);
 
   // effects
   useEffect(() => {
     finSubject.current.subscribe((event: Event) => {
-      if (event.type === 'setting') {
+      handlers.current.forEach((eventHandler) => {
+        if (eventHandler.type === event.type) {
+          eventHandler.handler(event);
+        }
+      });
+    });
+
+    register({
+      type: 'setting',
+      handler: (event) => {
         setCurrentPath(event.currentPath);
-      }
+      },
     });
     setIsLoading(false);
   }, []);
@@ -56,7 +75,13 @@ function FinProvider(props: FinProviderProps) {
   if (isLoading) return <></>;
 
   return (
-    <FinContext.Provider value={{ next: (event) => finSubject.current.next(event), currentPath }}>
+    <FinContext.Provider
+      value={{
+        next: (event) => finSubject.current.next(event),
+        currentPath,
+        register,
+      }}
+    >
       {children}
     </FinContext.Provider>
   );
